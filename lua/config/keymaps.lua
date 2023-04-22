854618962c0a70afab2b1de2dd377de3d3d79cf6
manyids2 <arun.mukundan@gmail.com>
2023-04-22 22:12:42 +0200

    Grep to quickfix list

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -95,3 +95,14 @@ map("n", "<leader>ub", function()
   end
   vim.o.background = background
 end, { desc = "Toggle background" })
+
+local trouble = require("trouble.providers.telescope")
+local telescope = require("telescope")
+telescope.setup({
+  defaults = {
+    mappings = {
+      i = { ["<c-t>"] = trouble.open_with_trouble },
+      n = { ["<c-t>"] = trouble.open_with_trouble },
+    },
+  },
+})

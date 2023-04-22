3c5c5d05f0fc75db855df5093a6683edb3e60285
manyids2 <arun.mukundan@gmail.com>
2023-04-22 23:11:31 +0200

    Turning on animation

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -16,7 +16,7 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.lang.json" },
     -- import/override with your plugins
     { import = "lazyvim.plugins.extras.ui.mini-starter" },
-    -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "plugins" },
   },
   defaults = {
